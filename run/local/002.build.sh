0e9b3dd4936bfe9db6c130f992ba4e0afcc0ca3c
cbw <cbwstar@naver.com>
2023-12-13 18:05:48 +0900

    빌드환경셋팅

--- run/local/002.build.sh
+++ run/local/002.build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 cd ../../
+git fetch --all
+git reset --hard origin/main
+git pull origin main
+
+chmod 755 ./run/local/*.sh
+
 gradle clean
 gradle build --exclude-task test
 
