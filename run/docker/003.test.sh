170196b7087ce918fa05f0ac82b66212a42b3e6e
cbw <cbwstar@naver.com>
2023-12-19 13:20:01 +0900

    빌드환경셋팅

--- run/docker/003.test.sh
+++ run/docker/003.test.sh
@@ -7,3 +7,10 @@ http://123.213.1.211:8080/composite
 curl http://123.213.1.211:8080/composite/9 | jq
 
 curl -X "DELETE" http://123.213.1.211:8080/composite/9
+
+
+
+curl --header "Content-Type: application/json" \
+   --request POST \
+   --data '{"productId":9, "productName":"9_name","productinfo":"9_productInfo","recommendList":[{"recommendId":91,"author":"Author91","content":"Content91"}],"reviewList":[{"reviewId":1,"author":"Author1","subject":"Subject1","content":"Content1"}]}' \
+http://localhost:8080/composite
